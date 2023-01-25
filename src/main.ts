5e281c8b3ef08b06c672e380ebc1c48f5d6837b7
Ewelina <ewe.zale@gmail.com>
2023-01-25 20:09:54 +0100

    import Pinia, FontAwesomeIcons and Router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faXmark, faBars } from "@fortawesome/free-solid-svg-icons";
+
+import "@/index.css";
+import router from "@/router/index.js";
 import App from "@/App.vue";
 
-createApp(App).mount("#app");
+library.add(faXmark);
+library.add(faBars);
+
+const pinia = createPinia();
+
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
