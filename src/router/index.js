7672584bc05d13e706cc7b5a729b9766286c7358
Ewelina <ewe.zale@gmail.com>
2023-01-25 20:12:58 +0100

    create Router and add home page

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+﻿import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomeView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior() {
+    return { top: 0, left: 0, behavior: "smooth" };
+  },
+});
+
+export default router;
