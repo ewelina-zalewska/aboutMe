dd2f9bdcad73b83fa4860e54a207d590d276f4ec
Ewelina <ewe.zale@gmail.com>
2023-01-25 19:47:23 +0100

    setup vitest

---
+++ tests/setup.ts
@@ -0,0 +1,9 @@
+﻿import { cleanup } from "@testing-library/vue";
+import matchers from "@testing-library/jest-dom/matchers";
+import { expect, afterEach } from "vitest";
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
