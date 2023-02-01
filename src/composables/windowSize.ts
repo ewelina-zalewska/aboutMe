6b51226aeebfbaeec9a834092b717ae13f08856d
Ewelina <ewe.zale@gmail.com>
2023-02-01 12:44:52 +0100

    window size update function

---
+++ src/composables/windowSize.ts
@@ -0,0 +1,28 @@
+﻿import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const windowSize = () => {
+  const windowWidth = ref(0);
+  const windowHeight = ref(0);
+
+  const updateWindowSize = () => {
+    windowWidth.value = window.innerWidth;
+    windowHeight.value = window.innerHeight;
+  };
+
+  onMounted(() => {
+    updateWindowSize();
+    window.addEventListener("resize", updateWindowSize);
+  });
+
+  onBeforeUnmount(() => {
+    window.removeEventListener("resize", updateWindowSize);
+  });
+
+  return {
+    windowWidth,
+    windowHeight,
+    updateWindowSize,
+  };
+};
+
+export default windowSize;
