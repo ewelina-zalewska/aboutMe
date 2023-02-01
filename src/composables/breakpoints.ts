2e9e20e4a3fad029400aa3530370a443ccd5a2e6
Ewelina <ewe.zale@gmail.com>
2023-02-01 12:47:42 +0100

    screen breakpoints for responsive menu

---
+++ src/composables/breakpoints.ts
@@ -0,0 +1,28 @@
+﻿import tailwindConfig from "tailwindcss/defaultConfig";
+import { computed } from "vue";
+import windowSize from "@/composables/windowSize";
+
+const breakpoints = () => {
+  const { windowWidth, windowHeight } = windowSize();
+  const breakpoint = 500;
+
+  const mdBreakpoint =
+    Number(tailwindConfig.theme.screens.md.replace("px", "")) || Number(768);
+  const lgBreakpoint =
+    Number(tailwindConfig.theme.screens.lg.replace("px", "")) || Number(1024);
+
+  const screenBreakpoint = computed(
+    () =>
+      (windowWidth.value >= mdBreakpoint && windowHeight.value >= breakpoint) ||
+      (windowWidth.value <= mdBreakpoint && windowHeight.value >= breakpoint)
+  );
+
+  return {
+    breakpoint,
+    mdBreakpoint,
+    lgBreakpoint,
+    screenBreakpoint,
+  };
+};
+
+export default breakpoints;
