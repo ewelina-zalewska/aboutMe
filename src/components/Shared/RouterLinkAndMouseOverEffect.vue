ac157b2a2d83754816fef6f6db33bd05d9e7ad50
Ewelina <ewe.zale@gmail.com>
2023-02-06 14:37:50 +0100

    onmouseover event

---
+++ src/components/Shared/RouterLinkAndMouseOverEffect.vue
@@ -0,0 +1,25 @@
+﻿<template>
+  <router-link
+    :to="{ path: pathName }"
+    @mouseover="MOUSEOVER"
+    @mouseleave="MOUSELEAVE"
+  >
+    <slot></slot>
+  </router-link>
+</template>
+
+<script lang="ts" setup>
+import type { PropType } from "vue";
+import { usePointerStore } from "@/stores/pointer";
+const pointerStore = usePointerStore();
+const MOUSEOVER = () => pointerStore.MOUSEOVER();
+const MOUSELEAVE = () => pointerStore.MOUSELEAVE();
+
+defineProps({
+  pathName: {
+    type: String as PropType<string>,
+    required: true,
+    default: "/",
+  },
+});
+</script>
---
+++ src/stores/pointer.ts
@@ -0,0 +1,17 @@
+﻿import { defineStore } from "pinia";
+import { ref } from "vue";
+export const MOUSEOVER = "MOUSEOVER";
+export const MOUSELEAVE = "MOUSELEAVE";
+
+export const usePointerStore = defineStore("pointer", () => {
+  const resizedEl = ref(false);
+
+  const MOUSEOVER = () => (resizedEl.value = true);
+  const MOUSELEAVE = () => (resizedEl.value = false);
+
+  return {
+    resizedEl,
+    MOUSEOVER,
+    MOUSELEAVE,
+  };
+});
