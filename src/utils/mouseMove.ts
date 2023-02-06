674b4dd040c145802be89da49794f26ee7680284
Ewelina <ewe.zale@gmail.com>
2023-02-06 14:34:19 +0100

    mouse move event listener

---
+++ src/utils/mouseMove.ts
@@ -0,0 +1,26 @@
+﻿import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const mouseMove = () => {
+  const clientx = ref(0);
+  const clienty = ref(0);
+
+  const updateMouseMove = ($event: MouseEvent) => {
+    clientx.value = $event.clientX;
+    clienty.value = $event.clientY;
+  };
+
+  onMounted(() => {
+    window.addEventListener("mousemove", updateMouseMove);
+  });
+
+  onBeforeUnmount(() => {
+    window.removeEventListener("mousemove", updateMouseMove);
+  });
+
+  return {
+    clientx,
+    clienty,
+  };
+};
+
+export default mouseMove;
