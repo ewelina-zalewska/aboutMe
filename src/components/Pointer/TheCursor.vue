d0c67c28aa7fab2f649ffb3f5861672bf3b5ee32
Ewelina <ewe.zale@gmail.com>
2023-02-06 14:32:10 +0100

    mousemove and mouseover effect

---
+++ src/components/Pointer/TheCursor.vue
@@ -0,0 +1,27 @@
+﻿<template>
+  <div
+    ref="cursorRef"
+    :style="{ left: x + 'px', top: y + 'px' }"
+    :class="[
+      'border-lg pointer-events-none z-[999] hidden h-10 w-10 rotate-45 rounded-full border bg-brand-creamy lg:absolute lg:block lg:mix-blend-difference',
+      { 'scale-[200%]': resizedEl },
+    ]"
+  ></div>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+import mouseMove from "@/utils/mouseMove";
+import { useElementSize } from "@vueuse/core";
+import { usePointerStore } from "@/stores/pointer";
+
+const { clientx, clienty } = mouseMove();
+const x = computed((): number => clientx.value - width.value / 2);
+const y = computed((): number => clienty.value - height.value / 2);
+
+const cursorRef = ref(null);
+const { width, height } = useElementSize(cursorRef);
+
+const pointerStore = usePointerStore();
+const resizedEl = computed(() => pointerStore.resizedEl);
+</script>
