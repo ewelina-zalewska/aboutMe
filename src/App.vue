315bab818af324b063cc2eeab8dbae02912cb6ff
Ewelina <ewe.zale@gmail.com>
2023-02-06 18:52:06 +0100

    detecting a mobile browser function

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div
-    class="fixed right-0 bottom-0 h-screen w-full bg-brand-creamy font-sans text-brand-darkblue lg:bg-brand-darkblue lg:text-brand-creamy"
+    class="fixed right-0 bottom-0 h-screen min-h-[280px] w-full min-w-[200px] bg-brand-creamy font-sans text-brand-darkblue lg:bg-brand-darkblue lg:text-brand-creamy"
   >
-    <the-cursor />
+    <the-cursor v-if="!isMobile()" />
     <main-nav />
     <router-view />
   </div>
 </template>
 
 <script lang="ts" setup>
-import TheCursor from "@/components/Shared/TheCursor.vue";
+import TheCursor from "@/components/Pointer/TheCursor.vue";
 import MainNav from "@/components/Navigation/MainNav.vue";
+
+const isMobile = () => {
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
 </script>
+
+<style>
+* {
+  cursor: none !important;
+}
+</style>
