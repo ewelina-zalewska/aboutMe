83e5833fb896f4dfa68d0c1046bb0b5f965cc88a
Ewelina <ewe.zale@gmail.com>
2023-02-08 13:56:51 +0100

    dark theme using CSS variables and local storage

---
+++ src/components/ModeChanger/DarkModeChanger.vue
@@ -0,0 +1,60 @@
+﻿<template>
+  <action-button @click="toggleTheme()" :btnClass="modeChangerStyle">
+    <template v-slot>
+      <font-awesome-icon
+        :icon="['fas', 'fa-lightbulb']"
+        :class="isSwitched"
+        aria-hidden="false"
+      />
+    </template>
+  </action-button>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed, onMounted } from "vue";
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import mobileMenuSize from "@/composables/mobileMenuSize";
+const { modeChangerStyle } = mobileMenuSize();
+
+const userTheme = ref("light-theme");
+
+const isSwitched = computed(
+  () =>
+    userTheme.value === "light-theme" && "text-theme-color-bulb animate-pulse"
+);
+
+const toggleTheme = () => {
+  const activeTheme = localStorage.getItem("user-theme");
+  if (activeTheme === "light-theme") {
+    setTheme("dark-theme");
+  } else {
+    setTheme("light-theme");
+  }
+};
+
+const setTheme = (theme: string) => {
+  localStorage.setItem("user-theme", theme);
+  userTheme.value = theme;
+  document.documentElement.className = theme;
+};
+
+const getMediaPreference = () => {
+  const hasDarkPreference = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  if (hasDarkPreference) {
+    return "dark-theme";
+  } else {
+    return "light-theme";
+  }
+};
+
+const getTheme = () => {
+  return localStorage.getItem("user-theme");
+};
+
+onMounted(() => {
+  const initUserTheme = getTheme() || getMediaPreference();
+  setTheme(initUserTheme);
+});
+</script>
