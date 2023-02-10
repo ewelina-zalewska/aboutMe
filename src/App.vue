019536bd6b49ec3459075bf6037c349f4725442f
Ewelina <ewe.zale@gmail.com>
2023-02-10 13:03:45 +0100

    create preloader: setInterval function and colors saved in localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <web-page-to-small-size v-if="isSmallSize" />
-  <web-page-container v-if="!isSmallSize" />
+  <div v-if="isLoading">
+    <the-preloader />
+  </div>
+  <div v-else>
+    <web-page-to-small-size v-if="isSmallSize" />
+    <web-page-container v-if="!isSmallSize" />
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, nextTick, onMounted } from "vue";
+import ThePreloader from "@/components/LoaderEvents/ThePreloader.vue";
 import WebPageContainer from "@/components/AboutMe/WebPageContainer.vue";
 import WebPageToSmallSize from "@/components/AboutMe/WebPageToSmallSize.vue";
+import { useDarkThemeStore } from "@/stores/darkTheme";
 import breakpoints from "@/composables/breakpoints";
+const darkThemeStore = useDarkThemeStore();
 const { isSmallSize } = breakpoints();
-</script>
 
-<style>
-* {
-  cursor: none !important;
-}
-</style>
+const isLoading = ref(true);
+
+onMounted(() =>
+  nextTick(() => {
+    const initUserTheme =
+      darkThemeStore.GET_THEME() || darkThemeStore.GET_MEDIA_PREFERENCE();
+    darkThemeStore.SET_THEME(initUserTheme);
+    // isLoading.value = false;
+    setTimeout(() => {
+      isLoading.value = false;
+    }, 3000);
+  })
+);
+</script>
---
+++ src/components/LoaderEvents/ThePreloader.vue
@@ -0,0 +1,32 @@
+﻿<template>
+  <div
+    class="flex h-screen w-screen flex-col items-center justify-center bg-theme-bg-color text-theme-text-color"
+  >
+    <div class="my-4 text-[40px] sm:text-[48px] md:text-[58px] lg:text-[64px]">
+      <span>{{ letter }}</span>
+      <span class="animate-pulse-fast"> | </span>
+    </div>
+    <div
+      class="z-[10000] inline-block h-16 w-16 after:m-2 after:block after:h-10 after:w-10 after:animate-spin-fast after:rounded-full after:border-[6px] after:border-colors-loader after:content-['']"
+    ></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+
+const letters: string[] = "Hello World".split("");
+
+const letter = ref("");
+const interval = ref<ReturnType<typeof setInterval>>();
+
+let i = 0;
+const addLetter = () => {
+  interval.value = setInterval(() => {
+    letter.value += letters[i++];
+    if (i === letters.length) clearInterval(interval.value);
+  }, 50);
+};
+
+onMounted(addLetter);
+</script>
