74816b8f33aeede9f078fb8b380442c20a092e73
Ewelina <ewe.zale@gmail.com>
2023-02-10 13:06:32 +0100

    dark theme in pinia store

--- src/components/ModeChanger/DarkModeChanger.vue
+++ src/components/ModeChanger/DarkModeChanger.vue
@@ -1,5 +1,8 @@
 ﻿<template>
-  <action-button @click="toggleTheme()" :btnClass="modeChangerStyle">
+  <action-button
+    @click="darkThemeStore.TOGGLE_THEME()"
+    :btnClass="modeChangerStyle"
+  >
     <template v-slot>
       <font-awesome-icon
         :icon="['fas', 'fa-lightbulb']"
@@ -11,50 +14,17 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed, onMounted } from "vue";
+import { computed } from "vue";
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import mobileMenuSize from "@/composables/mobileMenuSize";
+import { useDarkThemeStore } from "@/stores/darkTheme";
+
 const { modeChangerStyle } = mobileMenuSize();
+const darkThemeStore = useDarkThemeStore();
 
-const userTheme = ref("light-theme");
-
+const userTheme = computed(() => darkThemeStore.userTheme);
 const isSwitched = computed(
   () =>
     userTheme.value === "light-theme" && "text-theme-color-bulb animate-pulse"
 );
-
-const toggleTheme = () => {
-  const activeTheme = localStorage.getItem("user-theme");
-  if (activeTheme === "light-theme") {
-    setTheme("dark-theme");
-  } else {
-    setTheme("light-theme");
-  }
-};
-
-const setTheme = (theme: string) => {
-  localStorage.setItem("user-theme", theme);
-  userTheme.value = theme;
-  document.documentElement.className = theme;
-};
-
-const getMediaPreference = () => {
-  const hasDarkPreference = window.matchMedia(
-    "(prefers-color-scheme: dark)"
-  ).matches;
-  if (hasDarkPreference) {
-    return "dark-theme";
-  } else {
-    return "light-theme";
-  }
-};
-
-const getTheme = () => {
-  return localStorage.getItem("user-theme");
-};
-
-onMounted(() => {
-  const initUserTheme = getTheme() || getMediaPreference();
-  setTheme(initUserTheme);
-});
 </script>
---
+++ src/stores/darkTheme.ts
@@ -0,0 +1,49 @@
+﻿import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const TOGGLE_THEME = "TOGGLE_THEME";
+export const GET_MEDIA_PREFERENCE = "GET_MEDIA_PREFERENCE";
+export const GET_THEME = "GET_THEME";
+export const SET_THEME = "SET_THEME";
+
+export const useDarkThemeStore = defineStore("darkTheme", () => {
+  const userTheme = ref("dark-theme");
+
+  const TOGGLE_THEME = () => {
+    const activeTheme = localStorage.getItem("user-theme");
+    if (activeTheme === "light-theme") {
+      SET_THEME("dark-theme");
+    } else {
+      SET_THEME("light-theme");
+    }
+  };
+
+  const SET_THEME = (theme: string) => {
+    localStorage.setItem("user-theme", theme);
+    userTheme.value = theme;
+    document.documentElement.className = theme;
+  };
+
+  const GET_MEDIA_PREFERENCE = () => {
+    const hasDarkPreference = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (hasDarkPreference) {
+      return "dark-theme";
+    } else {
+      return "light-theme";
+    }
+  };
+
+  const GET_THEME = () => {
+    return localStorage.getItem("user-theme");
+  };
+
+  return {
+    userTheme,
+    TOGGLE_THEME,
+    GET_MEDIA_PREFERENCE,
+    GET_THEME,
+    SET_THEME,
+  };
+});
